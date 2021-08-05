14788f66f6f0c40f4d0692c7a34ef5f2dfc2deb0
Lucky <lucky_soft@163.com>
2021-08-05 19:03:06 +0800

    1, put file into hdfs 2, read hdfs files

---
+++ src/main/scala/com/labelhub/datalake/clean_data/ReadHDFSToHive.scala
@@ -0,0 +1,56 @@
+package com.labelhub.datalake.clean_data
+
+import com.fasterxml.jackson.databind.{DeserializationFeature, JsonNode}
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.labelhub.datalake.label_data.LabelDataHandler.DLabelDetail
+import org.apache.spark.sql.SparkSession
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.collection.mutable.ArrayBuffer
+
+object ReadHDFSToHive extends App {
+  val sparkSession: SparkSession = SparkSession.builder().appName("Read HDFS To Hive").master("spark://localhost:7077").getOrCreate()
+  val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  // val files = sparkSession.read.csv("hdfs://localhost:9000/user/hive/warehouse/csv/*.csv")
+  val files = sparkSession.read.format("CSV").option("header", "true").option("delimiter", ",").load("hdfs://localhost:9000/user/hive/warehouse/csv/*.csv")
+  var arr: ArrayBuffer[DLabelDetail] = ArrayBuffer()
+  var count : Integer = 0
+  files.collect().map(rootitem => {
+    val detail: JsonNode = mapper.readTree(rootitem.get(5).toString)
+    println(detail)
+    /*
+    val svgArr: ArrayNode = mapper.valueToTree(detail.get("svgArr"))
+    if (svgArr.size() > 0) {
+
+      svgArr.forEach(item => {
+        if (item.get("tool").asText() == "rectangle") {
+          count += 1
+          val xmin = List(item.get("data").get(0).get("x").asInt(), item.get("data").get(1).get("x").asInt(), item.get("data").get(2).get("x").asInt(), item.get("data").get(3).get("x").asInt()).min
+          val xmax = List(item.get("data").get(0).get("x").asInt(), item.get("data").get(1).get("x").asInt(), item.get("data").get(2).get("x").asInt(), item.get("data").get(3).get("x").asInt()).max
+          val ymin = List(item.get("data").get(0).get("y").asInt(), item.get("data").get(1).get("y").asInt(), item.get("data").get(2).get("y").asInt(), item.get("data").get(3).get("y").asInt()).min
+          val ymax = List(item.get("data").get(0).get("y").asInt(), item.get("data").get(1).get("y").asInt(), item.get("data").get(2).get("y").asInt(), item.get("data").get(3).get("y").asInt()).max
+          val width = xmax - xmin
+          val height = ymax - ymin
+          val color = item.get("color").asText()
+          val name = item.get("name").asText()
+          val isClosed = item.get("isClosed").asBoolean()
+          val isShow = if (item.hasNonNull("mustShow")) item.get("mustShow").asBoolean() else true
+          val uuid = item.get("uuid").asText()
+          val secondaryLabelId = 0
+          val labelIndex = 1
+          val tool = item.get("tool").asText()
+          val created_at = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(rootitem.get(7).asInstanceOf[Integer].toLong * 1000))
+
+          arr.append(DLabelDetail(count, xmin, ymin, color, name, isClosed, isShow, labelIndex, uuid, tool, secondaryLabelId, created_at, width, height))
+          println(arr)
+        }
+      })
+    }
+     */
+  })
+}
--- src/main/scala/com/labelhub/datalake/utils/ReadCSVToHDFS.scala
+++ src/main/scala/com/labelhub/datalake/utils/ReadCSVToHDFS.scala
@@ -1,6 +1,61 @@
 package com.labelhub.datalake.utils
 
-object ReadCSVToHDFS {
+import com.labelhub.datalake.utils.FilesOperation.del_dir
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs._
+import org.apache.spark.sql.SparkSession
 
+import java.io.{FileSystem => _, _}
+import java.io.File
 
+object ReadCSVToHDFS extends App {
+
+  def getFiles(file: File): Array[File] = {
+    val files = file.listFiles().filter(! _.isDirectory)
+      .filter(t => t.toString.endsWith(".csv"))
+    files ++ file.listFiles().filter(_.isDirectory).flatMap(getFiles)
+  }
+
+  def copyFileToHDFS(hdfs: FileSystem, localSource: String, hdfsTarget: String) = {
+    val targetPath = new Path(hdfsTarget)
+    if (!hdfsExists(hdfs, targetPath)) {
+      hdfsCreateFile(hdfs, targetPath)
+    }
+    val inputStream: FileInputStream = new FileInputStream(localSource)
+    val outputStream: FSDataOutputStream = hdfs.create(targetPath)
+    transport(inputStream, outputStream)
+  }
+
+  def hdfsExists(hdfs: FileSystem, name: Path): Boolean = {
+    hdfs.exists(name)
+  }
+
+  def hdfsCreateFile(hdfs: FileSystem, name: Path): Boolean = {
+    hdfs.createNewFile(name)
+  }
+
+  def transport(inputStream: InputStream, outputStream: OutputStream) = {
+    val buffer = new Array[Byte](64 * 1000)
+    var len = inputStream.read(buffer)
+    while (len != -1) {
+      outputStream.write(buffer, 0, len - 1)
+      len = inputStream.read(buffer)
+    }
+    outputStream.flush()
+    inputStream.close()
+    outputStream.close()
+  }
+
+  val sparkSession = SparkSession.builder()
+    .appName("Put File Into HDFS")
+    .master("spark://localhost:7077")
+    .getOrCreate()
+  val hadoopConf = sparkSession.sparkContext.hadoopConfiguration
+  val hdfs = FileSystem.get(hadoopConf)
+
+  val csvPath = Thread.currentThread().getContextClassLoader.getResource("csv_folder").getPath
+  val file = new File(csvPath)
+  getFiles(file).foreach(item => {
+    copyFileToHDFS(hdfs, item.getAbsolutePath, "/user/hive/warehouse/csv/" + item.getName)
+  })
 }
